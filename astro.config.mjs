e67590676cdd0de0db2b7186132ea26713b5f7b6
teapot-4l8 <chaiyunze@gmail.com>
2024-12-28 16:07:57 +0800

    fix pagae not found

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,7 +21,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://zycnotfound.vercel.app/",
+  site: "https://zycnotfound.netlify.app/",
   base: "/",
   trailingSlash: "always",
   integrations: [
